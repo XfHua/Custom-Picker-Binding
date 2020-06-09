016f8baea382d3777057e48a75e2989c77fe1231
XfHua <jasonHuaxf@163.com>
2020-06-09 14:27:35 +0800

    Add files via upload

---
+++ App267/App267/App267/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App267
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new testModel();
+           
+        }
+    }
+
+    public class testModel : INotifyPropertyChanged
+    {
+
+        public testModel() {
+
+            this.Title = "testTitle";
+            this.ItemsSource = new List<string>()
+            {
+                "1","2","3","4","5","6","7"
+            };
+        }
+
+        string title { get; set; }
+        public string Title
+        {
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return title;
+            }
+        }
+
+        IList itemsSource { get; set; }
+        public IList ItemsSource
+        {
+            set
+            {
+                if (itemsSource != value)
+                {
+                    itemsSource = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("ItemsSource"));
+                    }
+                }
+            }
+            get
+            {
+                return itemsSource;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ App267/App267/App267/View1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App267
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View1 : ContentView
+    {
+        public View1() => InitializeComponent();
+
+        public static BindableProperty TitleProperty = BindableProperty.Create(
+                propertyName: nameof(Title),
+                returnType: typeof(string),
+                declaringType: typeof(View1),
+                defaultValue: "",
+                defaultBindingMode: BindingMode.TwoWay);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            nameof(ItemsSource),
+            typeof(IList),
+            typeof(View1),
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: OnItemsSourceChanged);
+
+        static void OnItemsSourceChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var thisControl = bindable as View1;
+
+            if (oldValue != null)
+                thisControl.ItemsSource = oldValue as IList;
+
+            if (newValue != null)
+                thisControl.ItemsSource = newValue as IList;
+        }
+
+        public IList ItemsSource
+        {
+            get
+            {
+                var items = (IList)GetValue(ItemsSourceProperty);
+                return items;
+            }
+
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+    }
+}
